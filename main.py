0f3be05cddd986f58f3623e17c03016bd6808a59
leto <1907984744@qq.com>
2018-01-24 15:49:54 +0800

    initial work done

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from aimlcore import AimlParser
+# coding=utf-8
+from aimlCore import Kernel
+import os
 
+alice = Kernel()
+alice.learn('source/startup.xml')
+
+# 此处需要切换目录
+path = 'source'
+os.chdir(path)
+
+alice.respond('LOAD ALICE')
+
+while 1:
+    user_input = raw_input('输入>>')
+    response = alice.respond(user_input)
+    print response
