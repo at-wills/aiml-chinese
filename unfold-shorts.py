ac276e2415193176281be3a6fbf69773bee65e8a
leto <1907984744@qq.com>
2018-02-09 00:22:51 +0800

    finish unfold

--- unfold-shorts.py
+++ unfold-shorts.py
@@ -8,8 +8,8 @@
 
 import os
 from copy import deepcopy
-# from xml.etree import ElementTree
 from lxml import etree
+import itertools
 
 # 目标文件夹
 path = 'source'
@@ -19,48 +19,93 @@ os.chdir(path)
 
 
 def read_tree(in_file):
-    tree = etree.parse(in_file)
-    return tree
+    _tree = etree.parse(in_file)
+    return _tree
 
 
-def write_xml(tree, in_file):
+def write_xml(_tree, in_file):
     out_file = in_file[:-5] + '-unfold.aiml'
-    tree.write(out_file, encoding='utf-8', xml_declaration=True)
+    _tree.write(out_file, encoding='utf-8', xml_declaration=True)
 
 
-# 将 or 展开到多个 fold
-def unfold_or(fold):
-    ors_num = len(fold.findall('or'))
-    or_list = []
-    for index, _or in enumerate(fold.findall('or')):
-        or_list.append(_or.text)
-    for _or in or_list:
-        new_fold = etree.Element('fold')
-        new_fold.text = _or
-        fold.addnext(new_fold)
+class Patter:
+    # things： 'str' foldNode 'str' foldNode 'str'
+    def __init__(self, pattern):
+        self.things = []
+        if pattern.find('fold') is None:
+            if pattern.find('or') is not None:
+                print 'wrong syntax'
+                return
+            self.things.append(pattern.text)
+        else:
+            folds = pattern.findall('fold')
+            folds_num = len(folds)
+            for i in xrange(folds_num):
+                fold = folds[i]
+                if i == 0:
+                    pattern_text = fold.xpath('preceding-sibling::text()')[0].encode('utf-8').decode('utf-8')
+                else:
+                    pattern_text = fold.xpath('//text()[count(preceding-sibling::fold)=$count]', count=i)[0].encode(
+                        'utf-8').decode('utf-8')
+                self.things.append(pattern_text)
+                self.things.append(fold)
+                if i == folds_num - 1:
+                    last_pattern_text = fold.xpath('following-sibling::text()')[0].encode('utf-8').decode('utf-8')
+                    self.things.append(last_pattern_text)
+
+    # return a list containing all or.text
+    @staticmethod
+    def unfold_or(self, fold):
+        allowed_chars = set(' ' + '\n')
+        if len(fold.text) != 0:
+            if set(fold.text) > allowed_chars:
+                # 其实能做，不过…… (｀・ω・´)
+                print 'error: not support fold tag with text!'
+        or_list = []
+        for _or in fold.findall('or'):
+            or_list.append(_or.text)
+        return or_list
+
+    # return a list containing all unfolded patterns in only str
+    def get_str_patterns(self):
+        str_patterns = []
+        l = len(self.things)
+        for i in xrange(l):
+            item = self.things[i]
+            if type(item) is unicode:
+                self.things[i] = [item]
+            else:
+                self.things[i] = self.unfold_or(self, item)
+        for i in itertools.product(*self.things, repeat=1):
+            str_patterns.append(' '.join(i))
+        print str_patterns
+        return str_patterns
 
 
-def unfold(_category):
+def unfold_category(_category):
     if _category.find('pattern') is None or _category.find('pattern').find('fold') is None:
         return False, None
-    # 复制 category
-    category_copy = deepcopy(_category)
-    pattern = category_copy.find('pattern')
-    fold = category_copy.find('pattern').find('fold')
-    unfold_or(fold)
-    pattern.remove(fold)
-    return True, category_copy
+    pattern = _category.find('pattern')
+    str_patterns = Patter(pattern).get_str_patterns()
+    categories = []
+    for str_pattern in str_patterns:
+        category_copy = deepcopy(_category)
+        new_pattern = etree.Element('pattern')
+        new_pattern.text = str_pattern
+        pattern = category_copy.find('pattern')
+        pattern.addnext(new_pattern)
+        category_copy.remove(pattern)
+        categories.append(category_copy)
+    return True, categories
 
 
 tree = read_tree(unfold_list[0])
 root = tree.getroot()
 for category in root.iter('category'):
-    is_unfolded, new_category = unfold(category)
-    ever_unfolded = is_unfolded
-    # while is_unfolded:
-    #     is_unfolded, new_category = unfold(new_category)
-    if ever_unfolded:
+    is_unfolded, new_categories = unfold_category(category)
+    if is_unfolded:
         root.remove(category)
-        root.append(new_category)
+        for new_category in new_categories:
+            root.append(new_category)
 
 write_xml(tree, unfold_list[0])
